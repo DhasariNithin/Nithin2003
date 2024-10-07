10a32d2ffa6973e8e603d44cdd8ec8a04b687163
dhasarinithin7702@gmail.com <dhasarinithin7702@gmail.com>
2024-10-08 00:23:30 +0530

    Admit Controller

---
+++ Nithin2003/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nithin2003.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Errors", "Home");
+            }
+        }
+    }
+}
