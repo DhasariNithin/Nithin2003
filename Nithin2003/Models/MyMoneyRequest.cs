7f877197e2343e98fbf21d95a4cb030fd0bf76b3
dhasarinithin7702@gmail.com <dhasarinithin7702@gmail.com>
2024-10-12 15:28:02 +0530

    Added Data To MyMoneyRequest.cs

--- Nithin2003/Models/MyMoneyRequest.cs
+++ Nithin2003/Models/MyMoneyRequest.cs
@@ -2,6 +2,12 @@
 {
     public class MyMoneyRequest
     {
-
+        public int LoanId { get; set; }
+        public string RequestedUsername { get; set; }
+        public int LoanAmount { get; set; }
+        public string UserComment { get; set; }
+        public DateOnly LoanRequestedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public string AdminComment { get;set; }
     }
 }
