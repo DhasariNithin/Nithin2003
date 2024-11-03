dfc542b76150bb3f6f5a24cd4a04427dd6ba1f0e
Nithin <my dell>
2024-11-03 23:48:01 +0530

    Commit All

--- Nithin2003/Controllers/UsersController.cs
+++ Nithin2003/Controllers/UsersController.cs
@@ -267,27 +267,27 @@ namespace Nithin2003.Controllers
         // User Sign In
         public IActionResult SignIn()
         {
-            try
-            {
+            //try
+            //{
                 if (HttpContext.Session.GetString("SignIn") == "True")
                 {
                     return RedirectToAction("Index", "Dashboard");
                 }
 
                 return View();
-            }
-            catch (Exception ex)
-            {
-                return RedirectToAction("Errors", "Home");
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    return RedirectToAction("Errors", "Home");
+            //}
         }
 
         // validating the user with credentials
         [HttpPost]
         public IActionResult SignIn(MySignIn mySignIn)
         {
-            try
-            {
+            //try
+            //{
                 if (ModelState.IsValid)
                 {
                     var _user = _db.Users.Find(mySignIn.Username);
@@ -372,11 +372,11 @@ namespace Nithin2003.Controllers
                     return View();
                 }
 
-            }
-            catch (Exception ex)
-            {
-                return RedirectToAction("Errors", "Home");
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    return RedirectToAction("Errors", "Home");
+            //}
         }
 
         // User sign-out amd remove seesions
@@ -541,21 +541,7 @@ namespace Nithin2003.Controllers
             {
                 return RedirectToAction("Errors", "Home");
             }
-        }
-        //public IActionResult ContentEdit(string? Username)
-        //{
-        //    try
-        //    {
-        //        var _user = _db.Users.Find(Username);
-        //        _user.ContentEditor = true;
-        //        _db.Users.Update(_user);
-        //        _db.SaveChanges();
-        //        return RedirectToAction("Index", "Admin");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return RedirectToAction("Errors", "Home");
-        //    }
-        //}
+        }  
+        
     }
 }
