18c3c05078295c84242ea2dda376114aee9e4233
Nithin <my dell>
2025-02-13 18:06:52 +0530

    Google pay integration

---
+++ Nithin2003/Controllers/GooglePayController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nithin2003.Controllers
+{
+    public class GooglePayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
